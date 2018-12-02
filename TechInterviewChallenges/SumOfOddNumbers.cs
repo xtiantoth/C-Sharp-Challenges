d454d186d6ce6c44a64026df331a063923f08e25
Krisztian Toth <xtiantoth@gmail.com>
2018-12-02 18:47:51 +0100

    Technical interview question #1

---
+++ TechInterviewChallenges/SumOfOddNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArraysOddNumbers
+{
+    public static class SumOfOddNumbers
+    {
+        public static int AddOddNumbers(int[] myArray)
+        {
+            int odds = 0;
+
+            foreach(int number in myArray)
+            {
+                if (number % 2 == 0)
+                {
+
+                }
+                else
+                {
+                    odds += number;
+                }
+            }
+            return odds;
+                }
+    }
+}
