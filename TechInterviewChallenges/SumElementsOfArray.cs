41b0a356a9cdcfdf71c50db56353975cd615e0c1
Krisztian Toth <xtiantoth@gmail.com>
2018-12-02 20:25:05 +0100

    Tech Interview Challenge #2

---
+++ TechInterviewChallenges/SumElementsOfArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SumElementsOfArray
+{
+    public static class SumOfNumbers
+    {
+        public static long AddNumbers(int[] myArray)
+        {
+            long sum = 0;
+            foreach (int number in myArray)
+            {
+               
+                    sum += number;
+                
+            }
+            return sum;
+        }
+    }
+}
