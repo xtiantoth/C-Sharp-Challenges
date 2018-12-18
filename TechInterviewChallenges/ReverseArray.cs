d8d06fd1ad14c256e3f1102fe3f652053db2b02f
Krisztian Toth <xtiantoth@gmail.com>
2018-12-18 16:00:19 +0100

    Reverse array challenge

---
+++ TechInterviewChallenges/ReverseArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = { 1, 2, 3, 4, 5, 6 };
+            var rev = sequence.Reverse().ToArray();
+           
+            for (int i = 0; i < rev.Length; i++)
+            {
+                Console.WriteLine(rev[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
